f0dfeb2b7c5a5b6ec400e2252944a325af8286b1
shreyabehl <75259564+shreyabehl@users.noreply.github.com>
2025-02-07 16:39:03 +0530

    Solution to Input and Print Two Strings

---
+++ Input and Print Two Strings@@6788d5012db25efec946f940/code.c
@@ -0,0 +1,10 @@
+
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);  // Do integers input lena
+    printf("Sum: %d\n", a + b);  // Dono ka sum print karna
+    return 0;
+}
