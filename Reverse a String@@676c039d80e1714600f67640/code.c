ad9df80523daa11f50292fdcd6addac27cb1ee9b
Shreya Behl <75259564+shreyabehl@users.noreply.github.com>
2025-03-10 16:47:24 +0530

    Solution to Reverse a String

--- Reverse a String@@676c039d80e1714600f67640/code.c
+++ Reverse a String@@676c039d80e1714600f67640/code.c
@@ -1,13 +1,11 @@
+
+
 #include <stdio.h>
 #include <string.h>
-
-int main() {
-    int i, j;  
-    char a[100];  // 100 characters tak input le sakta hai
-
-    printf("Enter a string: ");
-    scanf("%s", a);  // ⚠️ `scanf()` sirf ek word lega (space ke baad ka ignore hoga)
-
+int main(){
+    char a[100];
+    printf("Enter a word: ");
+    scanf("%s", a);  
     printf("You entered: %s\n", a);
     return 0;
 }
