2cd8793bef8585c0aa68ae0640c2a583dc421adf
Shreya Behl <75259564+shreyabehl@users.noreply.github.com>
2025-03-15 16:26:25 +0530

    Solution to Find the Length of a String

---
+++ Find the Length of a String@@676c039d80e1714600f67643/code.c
@@ -0,0 +1,13 @@
+// Your code here...
+#include <stdio.h>
+#include <string.h>
+int main(){
+    int i, n, count = 0;
+
+    char a [100];
+    scanf(" %[^\n]", a);
+   n = strlen(a);
+   
+
+   printf("%d\n"); 
+}
