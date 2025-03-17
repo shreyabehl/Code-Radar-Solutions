cd6c50de01778260a175965763561087651197dd
Shreya Behl <75259564+shreyabehl@users.noreply.github.com>
2025-03-17 13:00:22 +0530

    Solution to Concatenate Two Strings

---
+++ Concatenate Two Strings@@676c039d80e1714600f67644/code.c
@@ -0,0 +1,25 @@
+// Your code here...
+
+
+
+#include <stdio.h>
+
+int main() {
+    char str1[50], str2[50], i, j;
+   
+   
+    scanf("%s", str1);
+    scanf("%s", str2);
+   
+
+    for (i = 0; a[i] != '\0'; ++i);
+    for (j = 0; str2[j]!='\0'; ++j, ++i) 
+    
+    {
+      str1[i]=str2[j];
+   }
+     str1[i]='\0';
+   printf("%s",str1);
+   
+   return 0;
+    }
