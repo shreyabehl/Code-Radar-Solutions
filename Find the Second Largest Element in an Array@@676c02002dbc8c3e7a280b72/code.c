1b036d10c56af839f588df423ec510eb184b8186
Shreya Behl <75259564+shreyabehl@users.noreply.github.com>
2025-04-07 17:09:15 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -4,105 +4,61 @@ int main() {
     int i, n;
     scanf("%d", &n);
     int arr[n];
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++){
         scanf("%d", &arr[i]);
-
     }
-    int max = arr[0];
-    for (i = 0; i < n; i++) {
-       if (arr[i] > max)
-       { max = arr[i];
-       }
+    for (i = 0; i < n; i++){
+        if (arr[i] > max) {
+            max = arr[i];
+
+        }
 
     }
     int found = 0;
     int second = max;
-
-    for (i = 0; i < n; i++) {
-        if (arr[i] < max) {
-            max = arr[i];
-        }
-    }
-    int found = 0;
-    int second = max;
-    for (i = 0; i < n; i++) {
-        if (arr[i] > max) {
-            if (found == 0) {
-                printf("-1\n");
-
-            }
-            else {
-                
-            }
-        }
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#include <stdio.h>
-
-int main() {
-    int n, i;
-    scanf("%d", &n);
-    int arr[n];
-
-    for (i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    int max = arr[0];
-    for (i = 1; i < n; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
-        }
-    }
-
     
-    int found = 0;
-    int second = max;
-
-    for (i = 0; i < n; i++) {
-        if (arr[i] < max) {
+    for ( i = 0; i < n; i++) {
+        if (arr[i] < max){
             if (found == 0) {
                 second = arr[i];
                 found = 1;
-            } else {
-                if (arr[i] > second) {
+            }
+            else{
+                if (arr[i] > second)
+                {
                     second = arr[i];
                 }
+
             }
+
         }
+
     }
 
     if (found == 0) {
         printf("-1\n");
-    } else {
-        printf("%d\n", second);
-    }
 
-    return 0;
+    }
+    else {
+        printf("%d\n", second);
+
+    }
 }
 
 
 
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
