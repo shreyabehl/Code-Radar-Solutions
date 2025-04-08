ff27161ffbb6bc91b9116cb6a26bd19aef763dbb
Shreya Behl <75259564+shreyabehl@users.noreply.github.com>
2025-04-08 12:01:14 +0530

    Solution to Find the Maximum Product of Two Elements in an Array

--- Find the Maximum Product of Two Elements in an Array@@676c02002dbc8c3e7a280b80/code.c
+++ Find the Maximum Product of Two Elements in an Array@@676c02002dbc8c3e7a280b80/code.c
@@ -1,55 +1,54 @@
-
 #include <stdio.h>
+
 int main() {
-    int i, n, j;
+    int i, n;
     scanf("%d", &n);
     int arr[n];
-    for (i = 0; i < n; i++){
+
+    for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
- int max1 = arr[0], min1 = arr[0];
- int max2 = arr[0], min2 = arr[1];
 
+    int max1 = arr[0], max2 = arr[0];
+    int min1 = arr[0], min2 = arr[0];
 
-
- for (i = 0; i < n; i++) {
-    if (arr[i] > max1){
-      max2 = max1;
-       max1 = arr[i];
-    }
-   
-   else {
+    // Find max1 and max2
+    for (i = 0; i < n; i++) {
+        if (arr[i] > max1) {
+            max2 = max1;
+            max1 = arr[i];
+        } else {
             if (arr[i] > max2) {
                 if (arr[i] < max1) {
                     max2 = arr[i];
                 }
-
- 
- }
-   }
-
-
- for (i = 0; i < n; i++){
-    if (min1 > arr[i]){
-        min2 = min1;
-        min1 = arr[i];
-    }
-
-    else if (arr[i] < min2) {
-        if (arr[i] > min1) {
-            min2 = arr[i];
+            }
         }
     }
- }
 
- int a = max1 * max2;
- int b = min1 * min2;
- if (a > b) {
-printf(" %d\n", a);
- }
+    // Find min1 and min2
+    for (i = 0; i < n; i++) {
+        if (arr[i] < min1) {
+            min2 = min1;
+            min1 = arr[i];
+        } else {
+            if (arr[i] < min2) {
+                if (arr[i] > min1) {
+                    min2 = arr[i];
+                }
+            }
+        }
+    }
 
- else 
-    printf(" %d\n", b);
- 
-    
+    int a = max1 * max2;
+    int b = min1 * min2;
+
+    if (a > b)
+        printf("%d\n", a);
+    else
+        printf("%d\n", b);
+
+    return 0;
 }
+
+
